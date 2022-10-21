994a576ffaf9a7cfab66cf6d8d4f417db09cf235
Margarita <mbiyun@bk.ru>
2022-10-21 17:10:15 +0700

    ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "nick")
+{
+    Console.WriteLine("Hurrey, it's Nick");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine("username");
+}
